28eb740470b3dc655fdc02241cd782fa804a9b6e
yurachern <tchernykh.yurij@ukr.net>
2019-06-25 04:26:20 +0300

    initial project version

---
+++ WpfLaba6/WpfLaba6/ConverterEnabledButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfLaba6
+{
+    public class ConverterEnabledButton : IValueConverter
+    {
+        public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str1 = values as string;
+            return str1.Length > 0;
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfLaba6/WpfLaba6/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLaba6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OvalSkin()
+        {
+            ResourceDictionary newDictionary = new ResourceDictionary();
+            newDictionary.Source = new Uri("OvalSkin.xaml", UriKind.Relative);
+            this.Resources.MergedDictionaries[0] = newDictionary;         
+        }
+        private void RectangularSkin()
+        {
+            ResourceDictionary newDictionary = new ResourceDictionary();
+            newDictionary.Source = new Uri("RectangularSkin.xaml", UriKind.Relative);
+            this.Resources.MergedDictionaries[0] = newDictionary;           
+        }
+        
+        private void CbStyles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbStyles.SelectedIndex != -1)
+            {
+                switch (cbStyles.SelectedIndex)
+                {
+                    case 0:
+                        OvalSkin();
+                        break;
+                    case 1:
+                        RectangularSkin();
+                        break;
+                }
+            }
+        }
+    }
+}
